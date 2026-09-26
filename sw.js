b8c89de72fae65367fd676238a22522fe3a294b0
mrazriabdrazak-web <mrazriabdrazak@gmail.com>
2026-09-26 08:33:21 +0800

    Refresh cache for Dolphin Satu calibration fix
    Bump the service worker cache key so installed clients fetch the verified Dolphin Satu calibration data.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX='gof-tank-sounding-';
-const CACHE='gof-tank-sounding-v8.4.0';
+const CACHE='gof-tank-sounding-v8.4.1';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith(CACHE_PREFIX)&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
